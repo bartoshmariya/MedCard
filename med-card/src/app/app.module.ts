58e37dacab46e1eb35e4d70ab9cc2527a33149e6
rancerenly <rancerenly@gmail.com>
2022-10-29 17:06:19 +0700

    create component consultation-details

--- med-card/src/app/app.module.ts
+++ med-card/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConsultationDetailsComponent } from './consultation-details/consultation-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultationDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ med-card/src/app/consultation-details/consultation-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consultation-details',
+  templateUrl: './consultation-details.component.html',
+  styleUrls: ['./consultation-details.component.scss']
+})
+export class ConsultationDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
