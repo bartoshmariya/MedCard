01ea3fbfc9ec7bd8685aaabdb929a145fc908e17
bartoshmariya <bartoshmari@yandex.ru>
2022-11-02 20:31:15 +0700

    MVC-Flask-project: modified html-files

--- backend/models.py
+++ backend/models.py
@@ -9,7 +9,7 @@ class Consultation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, default=datetime.now())
     patient = db.Column(db.String(150), nullable=False)
-    # icd_id = db.Column(db.Integer, db.ForeingKey('icd.id'))
+    # icd_id = db.Column(db.Integer, db.ForeignKey('icd.id'))
     diagnosis = db.Column(db.String(150), nullable=False)
     recommendation = db.Column(db.Text)
     department = db.Column(db.String(100), nullable=False)
